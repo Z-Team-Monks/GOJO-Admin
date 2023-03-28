5efad6356d4506bc587cdca212e46d7373e86bb6
Lingerew711 <se.lingerew.getie@gmail.com>
2023-03-28 21:08:32 +0300

    [admin-login] login page

---
+++ components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="wrapper">
+        <div class="title-text">
+          <div class="title login">Login</div>
+        </div>
+
+        <div class="form-container">
+          <div class="form-inner">
+            <form action="#" class="login">
+              <div class="field">
+                <input id="uname" type="text" maxlength="22" required />
+                <label>User Name</label>
+              </div>
+              <div class="field box-pass">
+                <input
+                  id="lpass"
+                  class="password"
+                  type="password"
+                  maxlength="8"
+                  required
+                />
+                <label>Password</label>
+                <i class="icon-password mdi mdi-eye-off-outline"></i>
+              </div>
+              <div class="check-boxx">
+                <input type="checkbox" id="scales" name="scales" checked />
+                <label for="scales">Remember me</label>
+              </div>
+              
+              <div class="field">
+                <input class="btn-login" type="submit" value="Login" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style lang="scss" scoped></style>
